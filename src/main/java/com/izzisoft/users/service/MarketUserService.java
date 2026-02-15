b2953ad424cb88399d84b76478be22c707ebb693
Karlz-Bandz <karol2646@gmail.com>
2026-02-15 19:53:20 +0100

    Jwt token generation

--- src/main/java/com/izzisoft/users/service/MarketUserService.java
+++ src/main/java/com/izzisoft/users/service/MarketUserService.java
@@ -4,9 +4,11 @@ import com.izzisoft.users.dto.MarketUserLoginRequest;
 import com.izzisoft.users.dto.MarketUserRegisterRequest;
 import com.izzisoft.users.dto.MarketUserResponse;
 import com.izzisoft.users.exception.EmailAlreadyExistsException;
+import com.izzisoft.users.exception.EmailNotExistsException;
 import com.izzisoft.users.exception.UsernameAlreadyExistsException;
 import com.izzisoft.users.model.MarketUser;
 import com.izzisoft.users.repo.MarketUserRepo;
+import com.izzisoft.users.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,11 +26,13 @@ public class MarketUserService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final JwtService jwtService;
+
     public String loginUser(MarketUserLoginRequest marketUserLoginRequest) {
 
         MarketUser foundUser = marketUserRepo.findByEmail(marketUserLoginRequest.email())
                 .orElseThrow(
-                        () -> new EmailAlreadyExistsException("Email not exists in database!")
+                        () -> new EmailNotExistsException("Email not exists in database!")
                 );
 
         Authentication authentication = authenticationManager.authenticate(
@@ -36,7 +40,7 @@ public class MarketUserService {
         );
 
         if (authentication.isAuthenticated()) {
-            return "Login success!";
+            return jwtService.generateToken(foundUser);
         }
 
         return "Wrong password!";
