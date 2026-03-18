32a69d6c57293644d8e28da16a3722d80cb64144
Karlz-Bandz <karol2646@gmail.com>
2026-03-18 10:58:07 +0100

    Issuer creation change

--- src/main/java/com/izzisoft/users/security/TokenService.java
+++ src/main/java/com/izzisoft/users/security/TokenService.java
@@ -26,7 +26,7 @@ public class TokenService {
                 .collect(Collectors.joining(" "));
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
+                .issuer("user-service")
                 .issuedAt(now)
                 .expiresAt(now.plus(1, ChronoUnit.HOURS))
                 .subject(authentication.getName())
